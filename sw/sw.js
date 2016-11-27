ed20c8cd0fa8885a402ae03c16875ac3b0b2be9e
asbtho <a_thom89@hotmail.com>
2016-11-27 01:45:11 +0100

    .

--- sw/sw.js
+++ sw/sw.js
@@ -1,15 +1,44 @@
+var swAPIroot = "http://swapi.co/api/";
 var swAPI = "http://swapi.co/api/people?search=";
+
 $( document ).ready(function(){
   $("#search").bind("input", function(){
     $.getJSON(swAPI + $("#search").val(),function( data ) {
-      var items = [];
-      $("#textbox").empty();
-      $.each( data["results"][0], function( key, val ) {
-        items.push( "<li id='" + key + "'>" + key + " : " + val + "</li>" );
-      });
-      $( "<ul/>", {
-        html: items.join( "" )
-      }).appendTo("#textbox");
+      getPersonInfo(data.results[0]);
     });
   });
 });
+
+function getPersonInfo(obj){
+  var items = [];
+  $("#textbox").empty();
+  items.push( "<li id=personname >Name: " + obj.name + "</li>" );
+  items.push( "<li id=personheight >Height: " + obj.height + "</li>" );
+  items.push( "<li id=personmass >Mass: " + obj.mass + "</li>" );
+  items.push( "<li id=personbirthyear >Year of birth: " + obj.birth_year + "</li>" );
+  getPlanetObj(obj.homeworld);
+  $( "<ul/>", {
+    html: items.join("")
+  }).appendTo("#textbox");
+}
+
+function getPlanetObj(path){
+  $.getJSON(path, function(data) {
+    var items = [];
+    $("#planetbox").empty();
+    items.push( "<li id=personhomeworld >Homeworld: " + data.name + "</li>" );
+    $( "<ul/>", {
+      html: items.join("")
+    }).appendTo("#planetbox");
+  });
+}
+
+function getFilmObj(array){
+  var films = [];
+  $.each(array,function(i,path){
+    $.getJSON(path,function(data) {
+      array.push(data);
+    });
+  });
+  return films;
+}
