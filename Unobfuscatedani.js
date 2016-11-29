3436b45c4bc8729cdbadba1c4dae172d88ccc92a
asbtho <a_thom89@hotmail.com>
2016-11-29 17:56:48 +0100

    removed for now

--- Unobfuscatedani.js
+++ Unobfuscatedani.js
@@ -2,13 +2,13 @@ window.onload = function(){
 	writebox = document.getElementById("write");
 	cursor = document.getElementById("cursor");
 	writebox.innerHTML += "root@thom.dir:~$ ";
-	siteText = "./getEmail|Contact32: MFPXI2DPNU4DSQDIN52G2YLJNQXGG33N|||=";
+	siteText = "./getEmail|Contact32: MFPXI2DPNU4DSQDIN52G2YLJNQXGG33N";
 	chars = siteText.split("");
 	var i = 0;
 	setTimeout(function(){ animation = setInterval(function(){
 		switch(chars[i]){
 			case "|": writebox.innerHTML+="<br>root@thom.dir:~$ "; break;
-			case "=": writebox.innerHTML+='<a href="sw/sw.html">Star Wars API fiddle</a>'; break;
+			//case "=": writebox.innerHTML+='<a href="sw/sw.html">Star Wars API fiddle</a>'; break; //removed for now
 			default: writebox.innerHTML+=chars[i];
 		}
 		i++;
