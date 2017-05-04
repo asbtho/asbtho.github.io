c84bee9e9bf03603b3dfed121083cdc03c8f766d
asbtho <a_thom89@hotmail.com>
2017-05-04 12:01:56 +0200

    test2

--- Unobfuscatedani.js
+++ Unobfuscatedani.js
@@ -9,7 +9,7 @@ window.onload = function(){
 		switch(chars[i]){
 			case "|": writebox.innerHTML+="<br>root@thom.dir:~$ "; break;
 			//case "=": writebox.innerHTML+='<a href="sw/sw.html">Star Wars API fiddle</a>'; break; //removed for now
-			case "=": writebox.innerHTML+='<a href="youtube/index.html">React App with Youtube API</a>'; break;
+			case "=": writebox.innerHTML+='<a href="youtube/">React App with Youtube API</a>'; break;
 			default: writebox.innerHTML+=chars[i];
 		}
 		i++;
